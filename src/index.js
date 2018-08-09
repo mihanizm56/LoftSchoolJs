a4989f47052d9d0771b8313d7610b9c4fbb29bc3
mihanizm56 <m727507.56@mail.ru>
2018-08-10 01:47:48 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@
    addListener('click', document.querySelector('a'), () => console.log('...')) // должна добавить указанный обработчик кликов на указанный элемент
  */
 function addListener(eventName, target, fn) {
+  target.addEventListener(eventName,fn)
+  return
 }
 
 /*
@@ -20,6 +22,8 @@ function addListener(eventName, target, fn) {
    removeListener('click', document.querySelector('a'), someHandler) // должна удалить указанный обработчик кликов на указанный элемент
  */
 function removeListener(eventName, target, fn) {
+  target.removeEventListener(eventName, fn)
+  return 
 }
 
 /*
@@ -31,6 +35,8 @@ function removeListener(eventName, target, fn) {
    skipDefault('click', document.querySelector('a')) // после вызова функции, клики на указанную ссылку не должны приводить к переходу на другую страницу
  */
 function skipDefault(eventName, target) {
+  target.addEventListener(eventName, e => e.preventDefault())
+  return 
 }
 
 /*
@@ -42,6 +48,8 @@ function skipDefault(eventName, target) {
    emulateClick(document.querySelector('a')) // для указанного элемента должно быть сэмулировано события click
  */
 function emulateClick(target) {
+  target.dispatchEvent(new MouseEvent('click'));
+  return 
 }
 
 /*
@@ -54,6 +62,11 @@ function emulateClick(target) {
    delegate(document.body, () => console.log('кликнули на button')) // добавит такой обработчик кликов для body, который будет вызывать указанную функцию только если кликнули на кнопку (элемент с тегом button)
  */
 function delegate(target, fn) {
+  target.addEventListener('click', function (e) {
+    if (e.target.tagName === "BUTTON") {
+      fn()
+    }
+  })
 }
 
 /*
@@ -66,6 +79,11 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
+  //target.addEventListener('click', fn);
+  target.addEventListener("click", ()=>{
+    fn();
+    this.removeEventListener('click',fn)
+  });
 }
 
 export {
