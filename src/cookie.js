80314f09d3b667dbf6596fdcde34fcea56ae3fcd
mihanizm56 <m727507.56@mail.ru>
2018-08-18 17:12:22 +0300

    making 7.3

--- src/cookie.js
+++ src/cookie.js
@@ -16,7 +16,7 @@
  7.3: На странице должно быть текстовое поле для фильтрации cookie
  В таблице должны быть только те cookie, в имени или значении которых, хотя бы частично, есть введенное значение
  Если в поле фильтра пусто, то должны выводиться все доступные cookie
- Если дабавляемая cookie не соответсвуте фильтру, то она должна быть добавлена только в браузер, но не в таблицу
+ Если добавляемая cookie не соответствует фильтру, то она должна быть добавлена только в браузер, но не в таблицу
  Если добавляется cookie, с именем уже существующией cookie и ее новое значение не соответствует фильтру,
  то ее значение должно быть обновлено в браузере, а из таблицы cookie должна быть удалена
 
@@ -42,20 +42,55 @@ const addValueInput = homeworkContainer.querySelector('#add-value-input');
 const addButton = homeworkContainer.querySelector('#add-button');
 // таблица со списком cookie
 const listTable = homeworkContainer.querySelector('#list-table tbody');
+// массив кук
+let cookieArray = [];
 
-// при загрузке страницы
 
+// при загрузке страницы рендерим таблицу с куками и создаём блок для фильтрации
 addCokieInTable();
+const filterBlock = homeworkContainer.querySelector('#filter-block');
+const newList = document.createElement('ul');
+filterBlock.appendChild(newList);
 
-function addCokie(name, value) {
 
-  //console.log(`получили в addCokie name = ${name}, value = ${value}`);
+// функция для делегирования кликов
+function delegateClick(event) {
+
+  if (event.target.dataset.key) {
+    console.log('Сработало на удаление')
+    deleteCookie(event.target.dataset.key)
+    return
+  }
+
+  if (event.target.id == 'add-button') {
+    console.log('Сработало на добавление')
+    addCokie()
+    return
+  }
+}
+
+
+// функция для добавления кук
+function addCokie() {
+
+  let name = addNameInput.value;
+  let value = addValueInput.value;
 
   document.cookie = `${name}=${value}`;
   addCokieInTable();
 }
 
 
+// функция для удаления кук
+function deleteCookie(number) {
+
+  document.cookie = number + `=;expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+
+  addCokieInTable();
+}
+
+
+// функция превращения кук в объект
 function getCookiesInObject() {
   return document.cookie
       .split('; ')
@@ -68,13 +103,20 @@ function getCookiesInObject() {
 }
 
 
-
-// функция рендеринга
+// функция обновления таблицы
 function addCokieInTable() {
   //console.log(`получили в addCokieInTable name = ${name}, value = ${value}`);
   const cookieObject = getCookiesInObject();
-  listTable.innerHTML = '';
-  for (let key in cookieObject) {
+  listTable.innerHTML = ''; //очищаем каждый раз поля таблицы перед загрузкой куки
+  
+  for (let key in cookieObject) {    
+
+    if (!cookieObject[key]){
+      listTable.innerHTML = '';
+      console.log('Куки пустые!')
+      return
+    }
+
     listTable.innerHTML += `
     <tr>
       <td class="first_td">${key}</td>
@@ -87,38 +129,47 @@ function addCokieInTable() {
 }
 
 
+// функция для кей-апа поисковика
+function onKeyUp() {
 
+  let cookieObject = getCookiesInObject();
+ 
+  if (!filterNameInput.value) {
+      newList.style.display = 'none'
+  } else {
+    newList.style.display = 'block'
 
+    for (let key in cookieObject) { 
+      cookieArray
+        .filter(key => isMatching(key, filterNameInput.value))
+        .map(key => `${key}`)
 
-function deleteCookie(event) {
-
-  if (event.target.dataset.key) {
-    document.cookie = event.target.dataset.key + `=;expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
-  }
-  addCokieInTable();
+      createFilterList(cookieObject) 
+    }
+  }  
 }
 
-  listTable.addEventListener('click', () => {
-    deleteCookie(event)
-  });
+
+// функция для создания поискового списка
+function createFilterList(what) {
+  newList.innerHTML = '';
+  newList.style.border = '1px solid grey';
+  console.log(`what = ${what}, length of what = ${what.length}`)
+
+  for (let i = 0; i < what.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = what[i];
+    newList.appendChild(li);
+  }
+}
 
 
-console.log(getCookiesInObject())
-
-
-
-
-
-
-
-
-
+//блок слушателей
 filterNameInput.addEventListener('keyup', function() {
-    // здесь можно обработать нажатия на клавиши внутри текстового поля для фильтрации cookie
+  onKeyUp()
 });
 
-addButton.addEventListener('click', () => {
-  addCokie(addNameInput.value, addValueInput.value)
-  //deleteCookie(addNameInput.value)
+document.addEventListener('click', (event) => {
+  delegateClick(event)
 });
 
