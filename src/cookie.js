15673c13cae8c7fd4bf56298cd2ee86589e0e256
mihanizm56 <m727507.56@mail.ru>
2018-09-21 00:56:18 +0300

    fixed

--- src/cookie.js
+++ src/cookie.js
@@ -5,17 +5,27 @@ let addValueInput = homeworkContainer.querySelector('#add-value-input');
 let addButton = homeworkContainer.querySelector('#add-button');
 let listTable = homeworkContainer.querySelector('#list-table tbody');
 
-function getCookies() {
-  return document.cookie
-    .split('; ')
-    .filter(Boolean)
-    .map(cookie => cookie.match(/^([^=]+)=(.+)/))
-    .reduce((obj, [, name, value]) => {
-      obj[name] = value;
+// function getCookies() {
+//   return document.cookie
+//     .split('; ')
+//     .filter(Boolean)
+//     .map(cookie => cookie.match(/^([^=]+)=(.+)/))
+//     .reduce((obj, [, name, value]) => {
+//       obj[name] = value;
 
-      return obj;
-    }, {});
+//       return obj;
+//     }, {});
+// }
+
+const getCookiesTest = () => {
+  let obj = {}
+  document.cookie.split('; ').reduce((prev, current) => {
+    const [name, value] = current.split('=')
+    obj[name] = value
+  }, {})
+  return obj
 }
+
 
 function isMatching(full, chunk) {
   return full.toUpperCase().indexOf(chunk.toUpperCase()) > -1;
@@ -27,7 +37,7 @@ renderTable();
 // Удаление
 listTable.addEventListener('click', function (e) {
   console.log(e.target);
-  if (e.target.dataset.key) {
+  if (e.target.className = 'delete') {
     document.cookie = e.target.dataset.key + `=;expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
   }
   renderTable();
@@ -46,11 +56,11 @@ addButton.addEventListener('click', () => {
 
 // функция рендеринга
 function renderTable() {
-  const cookie_obj = getCookies();
+  const cookie_obj = getCookiesTest();
   listTable.innerHTML = '';
   for (let key in cookie_obj) {
     if (!(isMatching(key, filterNameInput.value) || isMatching(cookie_obj[key], filterNameInput.value))) continue;
-    listTable.innerHTML += `<tr><td class="first_td">${key}</td><td>${cookie_obj[key]}</td><td><button class="del" 
+    listTable.innerHTML += `<tr><td class="first_td">${key}</td><td>${cookie_obj[key]}</td><td><button class="delete" 
         data-key="${key}">Удалить</button></td></tr>`;
   }
 }
